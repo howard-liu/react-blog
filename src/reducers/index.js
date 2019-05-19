e450a70eabcbe3ce7465b111522318cfe9794983
Howard Liu <hwd.liu@gmail.com>
2019-05-19 19:29:31 +1000

    app complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
-import { combineReducers} from 'redux';
+import { combineReducers } from "redux";
+import postReducer from "./postReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
-    replaceMe : () => 'replace me'
+  posts: postReducer,
+  users: usersReducer
 });
